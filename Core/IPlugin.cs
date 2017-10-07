aac99d9c9e0c08208108f3365c19ea88b060375c
LegitSoulja <macballerboy@gmail.com>
2017-10-07 19:51:58 -0400

    ISettings,IPlugin is now built with FruityUI.Core

---
+++ Core/IPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruityUI
+{
+    public interface IPlugin : IDisposable
+    {
+        string name { get; }
+        string author { get; }
+        string description { get; }
+        new void Dispose();
+    }
+}
--- Core/ISettings.cs
+++ Core/ISettings.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace FruityUI
 {
-
     public interface ISettings
     {
         string database { get; }
     }
-
 }
